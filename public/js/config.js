6fb37072fb48fd8aa2e23432447cf78919db49f8
kinenzarg-droid <kinenz.arg@gmail.com>
2026-08-20 16:50:39 -0300

    Add files via upload

--- public/js/config.js
+++ public/js/config.js
@@ -4,4 +4,5 @@ window.SURCOGS_CONFIG = {
   SUPABASE_URL: "https://cdrexpdauogollnqvmoe.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_1ilcI6-9X0538e0-LgRs9Q_SpOHI5SK",
   ADMIN_ID: "4ba159bb-aa5d-4743-811d-5de5db97da8e", // usuario admin (moderación)
+  WHATSAPP: "5491123986959",                       // botón de contacto, formato wa.me
 };
